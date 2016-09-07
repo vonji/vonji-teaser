a6f4f9f5bec39f19836aa9ac01dbcbc1155d67f2
Loup Peluso <loup.peluso@gmail.com>
2016-09-08 00:03:13 +0200

    fuck animation

--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -1,45 +1,107 @@
 
 <template lang="html">
-  <form v-on:submit.prevent class="inline">
-    <input transition="subscribe" placeholder="Votre nom"/>
-    <button>
-      <i class="fa fa-fw fa-angle-right fa-2x"></i>
-    </button>
-  </form>
+  <div style="min-width: 500px">
+    <form v-on:submit.prevent class="inline">
+      <input
+        v-if="this.step === 0" transition="subscribe"
+        type="text" placeholder="Votre nom"/>
+      <input
+        v-if="this.step === 1" transition="subscribe"
+        type="text" placeholder="Votre email"/>
+      <input
+        v-if="this.step === 2" transition="subscribe"
+        type="text" placeholder="Ce que vous savez faire"/>
+      <button @click="next">
+        <i class="fa fa-fw fa-angle-right fa-2x"></i>
+      </button>
+    </form>
+    <div class="bullets">
+      <i :class="bulletClass(0)"></i>
+      <i :class="bulletClass(1)"></i>
+      <i :class="bulletClass(2)"></i>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      status: 'init',
+      step: 0,
       email: '',
       name: '',
       skill: '',
+      currentInput: 'EmailInput',
     };
   },
-  computed: {},
+  computed: {
+  },
   ready() {},
   attached() {},
-  methods: {},
-  components: {},
+  methods: {
+    bulletClass(index) {
+      return ['fa', index === this.step ? 'fa-circle' : 'fa-circle-o'];
+    },
+    next() {
+      this.step = (this.step + 1) % 3;
+      switch (this.step) {
+        case 0: { this.currentInput = 'EmailInput'; break; }
+        case 1: { this.currentInput = 'NameInput'; break; }
+        case 2: { this.currentInput = 'SkillInput'; break; }
+        default: { this.currentInput = 'EmailInput'; }
+      }
+    },
+  },
+  components: {
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/settings.scss";
 
+.bullets {
+  margin-top: 5px;
+  display: flex;
+  justify-content: center;
+  i {
+    margin: 10px;
+  }
+}
+
+form {
+  display: flex;
+}
+
+input {
+  flex: 1;
+}
+
 i {
   color: $border-color;
 }
 
-.subscribe-transition {
+@keyframes fadeInRight {
+  from {
+    transform: translate3d(-100%, 0, 0);
+  }
+  to {
+  }
+}
+@keyframes fadeOutLeft {
+  from {
+  }
+  to {
+    transform: translate3d(100%, 0, 0);
+  }
+}
 
+.subscribe-transition {
 }
 .subscribe-enter {
-
+  animation: fadeInRight 2s;
 }
 .subscribe-leave {
-
+  animation: fadeOutLeft 2s;
 }
 </style>
