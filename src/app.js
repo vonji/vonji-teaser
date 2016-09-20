ff02f421171399f43541e0a5356ed40997630f2d
Loup Peluso <loup.peluso@gmail.com>
2016-09-20 10:12:16 +0200

    fix typo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App/app.jsx';
+import App from './components/App/App.jsx';
 
 const fb = require('./firebase.js');
 
