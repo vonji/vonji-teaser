50c23d4cd2ddd64b7e1dcb57e16c492b9acddab0
Loup Peluso <loup.peluso@gmail.com>
2016-09-22 00:26:25 +0200

    Fix bug in firebase env

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 const firebase = require('firebase/app');
 require('firebase/database');
 
-const PROD = process.argv.includes('--release');
+const PROD = process.env.NODE_ENV === 'production';
 
 if (PROD) {
   firebase.initializeApp(require('./firebase.config.prod.js'));
 } else {
+  console.log('Firebase in DEV mode');
   firebase.initializeApp(require('./firebase.config.dev.js'))
 }
 
